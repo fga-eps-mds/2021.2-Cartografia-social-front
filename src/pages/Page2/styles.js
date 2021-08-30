3fcbac38a2ee1be8e3196d140b02fcdb624bd461
Guilherme Banci <guibanci@gmail.com>
2021-08-29 23:47:51 -0300

    adding pages
    Signed-off-by: Guilherme Banci <guibanci@gmail.com>

---
+++ src/pages/Page2/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding: 20px;
+`;
+
+export const FirstInput = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const InputView = styled.View`
+  flex: 1;
+`;
