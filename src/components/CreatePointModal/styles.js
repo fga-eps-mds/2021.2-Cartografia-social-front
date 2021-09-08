b1a6a66d38e28ffd9fa1416c8b7e3ec16fae998b
Guilherme Banci <guibanci@gmail.com>
2021-09-08 19:23:59 -0300

    [#5] feat - creating marker modal
    Co-authored-by: Alexandre Miguel <aleronupe@gmail.com>

---
+++ src/components/CreatePointModal/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5';
+import theme from 'theme/theme';
+
+export const Icon = styled(FontAwesomeIcon).attrs({
+  color: theme.colors.white,
+})``;
+
+export const Container = styled.View`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: 20px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Option = styled.TouchableHighlight.attrs({
+  underlayColor: theme.colors.secondary,
+})`
+  border-radius: 100px;
+  width: ${({size}) => size}px;
+  height: ${({size}) => size}px;
+  background-color: ${theme.colors.primary};
+  align-items: center;
+  justify-content: center;
+  margin: 5px;
+`;
