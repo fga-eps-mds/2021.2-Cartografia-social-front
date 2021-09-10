d1538d1b3398096d1607ad6f36758d02f7f3578f
Guilherme Banci <guibanci@gmail.com>
2021-09-09 23:51:50 -0300

    [#14] feat - initial screen
    Co-authored-by: Arthur Rodrigues <arthurarp97@gmail.com>
    Signed-off-by: Guilherme Banci <guibanci@gmail.com>

---
+++ src/pages/InitialPage/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Btn from 'components/UI/Btn';
+import Text from 'components/UI/Text';
+import theme from 'theme/theme';
+import {Container, Logo} from './styles';
+
+// import {useDispatch} from 'react-redux';
+// import * as Actions from 'store/actions';
+
+const InitialPage = ({navigation}) => {
+  // const dispatch = useDispatch();
+
+  const navigateToScreen = async (screen) => {
+    navigation.navigate(screen);
+  };
+
+  return (
+    <Container>
+      <Text
+        alignSelf="center"
+        fontSize={theme.font.sizes.L}
+        fontWeight="bold"
+        m={3}>
+        Nova Cartografia Social
+      </Text>
+      <Logo />
+      <Btn
+        style={{marginVertical: 10}}
+        title="Modo de demonstração"
+        onPress={() => navigateToScreen('Map')}
+      />
+      <Btn
+        style={{marginVertical: 10}}
+        title="Login"
+        onPress={() => navigateToScreen('')}
+      />
+      <Btn
+        style={{marginVertical: 10}}
+        title="Solicitar apoio"
+        onPress={() => navigateToScreen('')}
+      />
+    </Container>
+  );
+};
+
+export default InitialPage;
