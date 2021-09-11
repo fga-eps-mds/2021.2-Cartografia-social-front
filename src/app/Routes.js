345bb507e82ac3dba4462a5fb096137ec02391c0
Mikhaelle <mikhabueno@outlook.com>
2021-09-11 18:14:14 -0300

    [#11] fix eslint problems
    Co-authored-by: Marco Antonio <markinlimac@gmail.com>

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -1,49 +1,55 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
+import { StatusBar } from 'react-native';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {ThemeProvider} from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ThemeProvider } from 'styled-components/native';
 // import {useSelector} from 'react-redux';
 // import {auth} from 'store/selectors';
 
 import theme from 'theme/theme';
 
-import Page1 from 'pages/Page1';
+import Page1 from 'pages/LoginPage';
 
 import Map from 'pages/Map';
 import InitialPage from 'pages/InitialPage';
 
 const Routes = () => {
-  // const user = useSelector(auth);
+    // const user = useSelector(auth);
 
-  const Stack = createStackNavigator();
+    const Stack = createStackNavigator();
 
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <StatusBar
-          barStyle="dark-content"
-          backgroundColor={theme.colors.white}
+    return ( <
+        >
+        <
+        ThemeProvider theme = { theme } >
+        <
+        StatusBar barStyle = "dark-content"
+        backgroundColor = { theme.colors.white }
+        /> <
+        NavigationContainer theme = {
+            { colors: { background: theme.colors.white } } } >
+        <
+        Stack.Navigator initialRouteName = "InitialPage" >
+        <
+        Stack.Screen name = "Map"
+        component = { Map }
+        options = {
+            { header: () => null } }
+        /> <
+        Stack.Screen name = "InitialPage"
+        component = { InitialPage }
+        options = {
+            { header: () => null } }
+        /> <
+        Stack.Screen name = "Page1"
+        component = { Page1 }
+        /> <
+        /Stack.Navigator> <
+        /NavigationContainer> <
+        /ThemeProvider> <
         />
-        <NavigationContainer theme={{colors: {background: theme.colors.white}}}>
-          <Stack.Navigator initialRouteName="InitialPage">
-            <Stack.Screen
-              name="Map"
-              component={Map}
-              options={{header: () => null}}
-            />
-            <Stack.Screen
-              name="InitialPage"
-              component={InitialPage}
-              options={{header: () => null}}
-            />
-            <Stack.Screen name="Page1" component={Page1} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </ThemeProvider>
-    </>
-  );
+    );
 };
 
 export default Routes;
