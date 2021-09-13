182d80f7cc106503c0714b232f83fecdcd50a936
Guilherme Banci <guibanci@gmail.com>
2021-09-13 18:40:34 -0300

    [#13] feat - show options for pick images
    Co-authored-by: Mikhaelle Bueno <mikhabueno@outlook.com>
    Signed-off-by: Guilherme Banci <guibanci@gmail.com>

---
+++ src/components/UI/FlatList/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+import theme from 'theme/theme';
+import {space, typography, flexbox} from 'styled-system';
+
+export default styled.FlatList.attrs({
+  showsHorizontalScrollIndicator: false,
+  showsVerticalScrollIndicator: false,
+})`
+  font-size: ${(props) =>
+    props.fontSize ? props.fontSize : theme.font.sizes.M}px;
+  color: ${(props) => (props.color ? props.color : theme.colors.black)};
+  ${space};
+  ${typography};
+  ${flexbox};
+`;
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,6 +1,7 @@
 export {default as Btn} from './Btn';
 export {default as Input} from './Input';
 export {default as ScrollView} from './ScrollView';
+export {default as FlatList} from './FlatList';
 export {default as Text} from './Text';
 export {default as View} from './View';
 export {default as Touchable} from './Touchable';
