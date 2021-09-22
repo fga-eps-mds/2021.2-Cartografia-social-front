621764dd914bf793e8c76652dfae473517222ec4
Mikhaelle <mikhabueno@outlook.com>
2021-09-21 21:12:12 -0300

    [#21] feat - update user location method
    Co-authored-by: Arthur Rodrigues <arthurarp97@gmail.com>

--- src/services/useLocation.js
+++ src/services/useLocation.js
@@ -1,53 +1,27 @@
 import {useEffect, useState} from 'react';
 import {Alert, Platform} from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
-import {request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import {PERMISSIONS} from 'react-native-permissions';
+import {checkPermission} from './checkPermission';
 
 export default () => {
   const [location, setLocation] = useState(null);
   const [hasLocationPermission, setHasLocationPermission] = useState(false);
 
   const requestLocation = () => {
-    let permission;
     if (Platform.OS === 'ios') {
-      permission = PERMISSIONS.IOS.LOCATION_ALWAYS;
+      checkPermission(PERMISSIONS.IOS.LOCATION_ALWAYS).then((result) => {
+        setHasLocationPermission(result);
+      });
     } else {
-      permission = PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION;
+      checkPermission(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then(
+        (result) => {
+          setHasLocationPermission(result);
+        },
+      );
     }
-
-    request(permission).then((result) => {
-      switch (result) {
-        case RESULTS.UNAVAILABLE:
-          Alert.alert(
-            'Atenção',
-            'This feature is not available (on this device / in this context)',
-          );
-          break;
-        case RESULTS.DENIED:
-          Alert.alert(
-            'Atenção',
-            'The permission has not been requested / is denied but requestable',
-          );
-          break;
-        case RESULTS.LIMITED:
-          Alert.alert(
-            'Atenção',
-            'The permission is limited: some actions are possible',
-          );
-          break;
-        case RESULTS.GRANTED:
-          setHasLocationPermission(true);
-          break;
-        case RESULTS.BLOCKED:
-          Alert.alert(
-            'Atenção',
-            'The permission is denied and not requestable anymore',
-          );
-          break;
-        default:
-      }
-    });
   };
+
   const getLocation = () => {
     if (hasLocationPermission) {
       Geolocation.getCurrentPosition(
