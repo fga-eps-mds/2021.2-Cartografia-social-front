e8f0965ae4c461d719d35d386af6ac57fefe3bb3
Marco Antonio <markinlimac@gmail.com>
2021-09-28 13:19:03 -0300

    [#20] test - fix tests
    Co-authored-by: Alexandre Miguel <aleronupe@gmail.com>

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -6,17 +6,25 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Map from '../src/pages/Map';
-// import {Provider} from 'react-redux';
-// import { createStore } from 'redux';
-// import {PersistGate} from 'redux-persist/integration/react';
-// import {persistor, store} from '../src/store/StoreProvider';
-
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import persistReducers from '../src/store/persistReducers';
+import reducers from '../src/store/reducers';
 // Note: test renderer must be required after react-native.
+jest.useFakeTimers()
 
-it('renders correctly', () => {
+const composeEnhancers = compose;
+
+const store = createStore(
+  persistReducers(reducers),
+  composeEnhancers(applyMiddleware(thunk)),
+);
+
+it('first test', () => {
   renderer.create(
-    // <Provider store={'store'}>
-          <Map  />
-    // </Provider>
-    );
+    <Provider store={store}>
+          <Map />
+      </Provider>
+  );
 });
