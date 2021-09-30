60d2fe56f10df070ac59c4248dbbd96f8f4cc173
Arthur <arthurarp@hotmail.com>
2021-09-30 13:36:01 -0300

    [#21] feat - Add conditional to save button that now only will be able after recording audio; Change save function to send to createPoint component the paths of recorded audios
    Co-authored-by: Mikhaelle Bueno <mikhabueno@outlook.com>
    Signed-off-by: Arthur <arthurarp@hotmail.com>

--- src/components/RecordAudioModalContent/index.js
+++ src/components/RecordAudioModalContent/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {Alert} from 'react-native';
 import { Text } from 'components/UI';
 import theme from 'theme/theme';
 import PropTypes from 'prop-types';
@@ -7,8 +8,7 @@ import AudioRecorderPlayer, {
   AudioSourceAndroidType,
 } from 'react-native-audio-recorder-player';
 import useRecordAudio from 'services/useRecordAudio';
-import instance from 'services/api2';
-import FormData from 'form-data';
+import RNFetchBlob from 'rn-fetch-blob'
 import Btn from '../UI/Btn';
 import {
   Container,
@@ -21,7 +21,7 @@ import {
 
 let audioRecorderPlayer = new AudioRecorderPlayer();
 
-const RecordAudioModalContent = ({ toggleModal }) => {
+const RecordAudioModalContent = ({ toggleModal, setAudios, value }) => {
   const { ableToRecord } = useRecordAudio();
   const [recordMinutesTime, setrecordMinutesTime] = useState('00');
   const [recordSecondsTime, setrecordSecondsTime] = useState('00');
@@ -31,25 +31,37 @@ const RecordAudioModalContent = ({ toggleModal }) => {
   const [hasAudio, setHasAudio] = useState(false);
   const [startPlay, setStartPlay] = useState(false);
   const [audioPath, setAudioPath] = useState('');
+  const [duration, setDuration] = useState(0);
 
   const audioSet = {
     AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
     AudioSourceAndroid: AudioSourceAndroidType.MIC,
   };
 
+  const dirs = RNFetchBlob.fs.dirs;
+  const path = Platform.select({
+    ios: 'hello.m4a',
+    android: `${dirs.CacheDir}/sound${value}.mp4`,
+  });
+
   // função para começar a gravar
   const onStartRecord = async () => {
+    if (value == 6) {
+      Alert.alert('Atingiu o limite máximo (5) de gravação de áudios na criação de um marcador')
+      toggleModal();
+      return;
+    }
     setRecording(true);
-    const path = await audioRecorderPlayer.startRecorder(undefined, audioSet);
+    const response = await audioRecorderPlayer.startRecorder(path, audioSet);
     audioRecorderPlayer.addRecordBackListener((e) => {
+      setDuration(e.currentPosition);
       const time = audioRecorderPlayer
         .mmssss(Math.floor(e.currentPosition))
         .split(':');
       setrecordMinutesTime(time[0]);
       setrecordSecondsTime(time[1]);
     });
-
-    setAudioPath(path);
+    setAudioPath(response);
   };
 
   const onStopRecord = async () => {
@@ -61,6 +73,7 @@ const RecordAudioModalContent = ({ toggleModal }) => {
 
   const deleteRecord = async () => {
     setHasAudio(false);
+    await audioRecorderPlayer.pausePlayer();
     audioRecorderPlayer = new AudioRecorderPlayer();
     setrecordMinutesTime('00');
     setrecordSecondsTime('00');
@@ -71,7 +84,7 @@ const RecordAudioModalContent = ({ toggleModal }) => {
   const onStartPlay = async () => {
     // function to play an audio after recording it.
     setStartPlay(true);
-    await audioRecorderPlayer.startPlayer();
+    await audioRecorderPlayer.startPlayer(path);
     audioRecorderPlayer.addPlayBackListener((e) => {
       if (e.currentPosition === e.duration) {
         audioRecorderPlayer.stopPlayer();
@@ -97,20 +110,14 @@ const RecordAudioModalContent = ({ toggleModal }) => {
   };
 
   const onSave = async () => {
-    toggleModal();
-    const formData = new FormData();
-    formData.append('name', 'new_audio');
-    formData.append('file', {
-      uri: audioPath,
+    objAudio = {
+      fileName: `sound${value}.mp4`,
       type: 'audio/mpeg',
-      name: 'last_recording',
-    });
-
-    await instance.post('midia/uploadMidia', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
+      uri: audioPath,
+      duration: duration,
+    }
+    setAudios([objAudio]);
+    toggleModal();
   };
 
   return (
@@ -186,7 +193,11 @@ const RecordAudioModalContent = ({ toggleModal }) => {
           color={theme.colors.primary}
           onPress={onCancel}
         />
-        <Btn title="Salvar" color="#FFF" onPress={onSave} />
+        {hasAudio ? (
+          <Btn title="Salvar" color="#FFF" onPress={onSave} />
+        ) : (
+          <Btn title="Salvar" background={theme.colors.grey} color="#FFF" onPress={() => {}} />
+        )}
       </OptionsButton>
     </Container>
   );
