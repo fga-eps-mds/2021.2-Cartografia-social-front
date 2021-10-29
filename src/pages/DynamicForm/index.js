b78950a030fad83e0512ae7ef18bcadffcd3dff5
Mikhaelle <mikhabueno@outlook.com>
2021-10-28 21:16:52 -0300

    [#69] fix - update form style
    Co-authored-by: Guilherme Banci <guibanci@gmail.com>
    Co-authored-by: Arthur Rodrigues <arthurarp97@gmail.com>

--- src/pages/DynamicForm/index.js
+++ src/pages/DynamicForm/index.js
@@ -4,31 +4,32 @@ import required from 'validators/required';
 import Btn from 'components/UI/Btn';
 import api from 'services/api';
 import {FlatList, Alert} from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
 import {Container, InputText} from './styles';
 
 const DynamicForm = () => {
   // Cria listas dos dados do form
   const [questionsFormList, setQuestionsFormList] = useState([]); // Formato: { question: '', id: ''}
   const [questionsStateList, setQuestionsStateList] = useState([]); // Formato: { value: '', isValid: Bool}
+  const [isLoading, setIsLoading] = useState(true);
 
   // Faz chama POST para obter dados do formulário
-  const getQuestionsToCreateCommunity = () => {
-    api.get('/community/questionsToCreateCommunity').then(
-      (response) => {
-        const listQuestions = response.data;
-        const inputList = listQuestions.map(() => ({
-          isValid: false,
-          value: '',
-        }));
-        setQuestionsStateList(inputList);
-        setQuestionsFormList(listQuestions);
-      },
-      () => {},
-    );
+  const getQuestionsToCreateCommunity = async () => {
+    await api.get('/community/questionsToCreateCommunity').then((response) => {
+      const listQuestions = response.data;
+      const inputList = listQuestions.map(() => ({
+        isValid: false,
+        value: '',
+      }));
+      setQuestionsStateList(inputList);
+      setQuestionsFormList(listQuestions);
+      setIsLoading(false);
+    });
   };
 
   // Primeira função a ser chamada
   useEffect(() => {
+    setIsLoading(true);
     getQuestionsToCreateCommunity();
   }, []);
 
@@ -58,7 +59,7 @@ const DynamicForm = () => {
     try {
       await api.post('/community/sendAnswers', {answers: sendFormList});
     } catch (error) {
-      Alert.alert('Cartografia Social', error.message);
+      Alert.alert('Cartografia Social', error);
     }
   };
 
@@ -72,11 +73,14 @@ const DynamicForm = () => {
   // Renderiza cada Item do Formulário
   const renderItem = ({item, index}) => (
     <>
-      <InputText>{item.question}</InputText>
+      <InputText>
+        {index + 1}
+        {'. '}
+        {item.question}
+      </InputText>
       <Input
         label={`Insira ${item.question}`}
         onChange={(value) => onChangeQuestion(value, index)}
-        // value={questionsStateList[index].value}
         autoCapitalize="words"
         rules={[required]}
       />
@@ -94,12 +98,17 @@ const DynamicForm = () => {
 
   return (
     <Container>
-      <FlatList
-        data={questionsFormList}
-        renderItem={renderItem}
-        key={(item) => item._id}
-        ListFooterComponent={renderButtom}
-      />
+      {isLoading ? (
+        <Spinner visible={isLoading} textContent="Carregando..." />
+      ) : (
+        <FlatList
+          data={questionsFormList}
+          renderItem={renderItem}
+          key={(item) => item._id}
+          keyExtractor={(item) => item._id}
+          ListFooterComponent={renderButtom}
+        />
+      )}
     </Container>
   );
 };
