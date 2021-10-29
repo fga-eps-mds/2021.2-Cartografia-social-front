c0c900c1d7c533dacfd630a5642a5534257426a6
Lorrany Azevedo <lorrany90@gmail.com>
2021-10-28 23:20:18 -0300

    [61] hotfix - fix profile page
    Co-authored-by: Guilherme Banci <guibanci@gmail.com>

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,18 +1,31 @@
-import React, {useState} from 'react';
-import ScrollView from 'components/UI/ScrollView';
-import {Container, Header, HeaderText} from './styles';
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import {View, Text} from 'components/UI';
+import {useSelector} from 'react-redux';
+import {auth} from 'store/selectors';
 
-const Profile = () => {
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+import {IconStyle} from './styles';
+
+// import ScrollView from 'components/UI/ScrollView';
+// import {Container, Header, HeaderText} from './styles';
+
+function Profile(props) {
+  const user = useSelector(auth);
+  console.log(user.data.name);
+
   return (
-    <>
-      <Header>
-        <HeaderText my={3}>Perfil de usuário</HeaderText>
-      </Header>
-      <ScrollView>
-        <Container />
-      </ScrollView>
-    </>
+    <DrawerContentScrollView {...props}>
+      <View alignItems="center" justifyContent="center" my={4}>
+        <IconStyle isPicture={false} user={user.data.name} />
+        <Text fontWeight="bold">{user.data.name}</Text>
+      </View>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
   );
-};
+}
 
 export default Profile;
