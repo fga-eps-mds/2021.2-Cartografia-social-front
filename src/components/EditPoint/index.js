ef62777bd4ea2c398602140dc98e1fee7221d389
Marco Antonio <markinlimac@gmail.com>
2021-10-29 20:24:47 -0300

    Adding edit marker page
    Co-authored-by: Mikhaelle Bueno <mikhabueno@outlook.com>

---
+++ src/components/EditPoint/index.js
@@ -0,0 +1,365 @@
+import React, {useRef, useMemo, useState, useEffect} from 'react';
+import {Alert} from 'react-native';
+import Modal from 'react-native-modal';
+import BottomSheet, {BottomSheetScrollView} from '@gorhom/bottom-sheet';
+import PropTypes from 'prop-types';
+import {Btn, Input, View, FlatList, Text} from 'components/UI';
+import required from 'validators/required';
+import {useDispatch, useSelector} from 'react-redux';
+import {auth} from 'store/selectors';
+import * as Actions from 'store/actions';
+import api from 'services/api';
+import Fabs from 'components/Fabs';
+import theme from 'theme/theme';
+import instance from 'services/api2';
+import useDocumentPicker from 'services/useDocumentPicker';
+import FormData from 'form-data';
+import RecordAudioModalContent from 'components/RecordAudioModalContent';
+import SelectModal from 'components/SelectModal';
+import ShowMediaModal from 'components/ShowMediaModal';
+import MediaModalContent from 'components/MediaModalContent';
+import ImageView from 'react-native-image-viewing';
+import UseCamera from '../../services/useCamera';
+import ImagePreview from '../ImagePreview';
+import AudioPreview from '../AudioPreview';
+import DocumentPreview from '../DocumentPreview';
+import VideoPreview from '../VideoPreview';
+
+import {Container, Icon} from './styles';
+
+const EditPoint = ({marker, locationSelected, show, onClose}) => {
+  UseCamera();
+  const dispatch = useDispatch();
+  const listMarkers = useSelector(state => state.markers.list)
+  const user = useSelector(auth);
+  const sheetRef = useRef(null);
+
+  let namePlaceholder = 'Digite aqui o título do novo ponto';
+  let descriptionPlaceholder = 'Digite aqui a descrição do novo ponto';
+  let buttonName = 'Salvar ponto';
+
+  const DEFAULT_TITLE_STATE = {
+    isValid: true,
+    value: marker.title,
+  };
+
+  const DEFAULT_DESCRIPTION_STATE = {
+    isValid: true,
+    value: marker.description,
+  };
+
+  const [title, setTitle] = useState(DEFAULT_TITLE_STATE);
+  const [description, setDescription] = useState(DEFAULT_DESCRIPTION_STATE);
+  const [audioCount, setAudioCount] = useState(0);
+  const [medias, setMedias] = useState(marker.multimedia);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalCamVisible, setModalCamVisible] = useState(false);
+  const [modalMediaVisible, setModalMediaVisible] = useState(false);
+  const [modalShowMediaVisible, setModalShowMediaVisible] = useState(false);
+  const [mediaShowed, setMediaShowed] = useState({});
+  const [visibleImageModal, setIsVisibleImageModal] = useState(false);
+  const [openedImage, setOpenedImage] = useState({});
+
+  const selectPdf = async () => {
+    const results = await useDocumentPicker();
+
+    if (results) {
+      const formattedResults = results.map((item) => ({
+        uri: item.uri,
+        fileName: item.name,
+        type: item.type,
+      }));
+
+      setMedias([...medias, ...formattedResults]);
+    }
+  };
+
+  const actions = [
+    {
+      icon: 'file-pdf',
+      onPress: () => selectPdf(),
+    },
+    {
+      icon: 'microphone',
+      onPress: () => setModalVisible(true),
+    },
+    {
+      icon: 'camera',
+      onPress: () => {
+        setModalCamVisible(true);
+      },
+    },
+    {
+      icon: 'paperclip',
+      onPress: () => {
+        setModalMediaVisible(true);
+      },
+    },
+  ];
+
+  const onSave = async () => {
+    var markerIndex = listMarkers.indexOf(marker);
+
+    updatedMarker = {
+      latitude: marker.latitude,
+      longitude: marker.longitude,
+      title: title.value,
+      description: description.value,
+      multimedia: medias,
+      id: marker.id,
+    }
+    
+    if (user && user.id) {
+      await api
+        .put('/maps/point', marker)
+        .catch((error) => {
+          Alert.alert('Cartografia Social', error.message);
+        });
+    }
+
+    medias.map(async (media) => {
+      let mediaId = '';
+
+      const formData = new FormData();
+      formData.append('file', {
+        uri: media.uri,
+        type: media.type,
+        name: media.fileName,
+      });
+
+      await instance
+        .post('midia/uploadMidia', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then((response) => {
+          mediaId = response.data;
+        })
+        .catch(() => {
+          Alert.alert('erro ao salvar áudio: ', media.fileName);
+        });
+      
+      const newMediaPoint = {
+        locationId: marker.id,
+        mediaId,
+      };
+      await api.post('/maps/addMediaToPoint', newMediaPoint).catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      });
+    });
+
+    
+    dispatch(Actions.updateMarker(updatedMarker, markerIndex));
+
+    // // sheetRef.current.close();
+
+    // setTimeout(() => {
+    //   onCloseBottomSheet();
+    // }, 1000);
+    // return locationSelected;
+  };
+
+  const formIsValid = () => {
+    return title.isValid;
+  };
+
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  const toggleCamModal = () => {
+    setModalCamVisible(!modalCamVisible);
+  };
+
+  const toggleMediaModal = () => {
+    setModalMediaVisible(!modalMediaVisible);
+  };
+
+  const closeShowMediaModal = () => {
+    setMediaShowed({});
+    setModalShowMediaVisible(false);
+  };
+
+  const handleShowMedia = (fileType, fileUri, fileDuration) => {
+    const media = {
+      type: fileType,
+      uri: fileUri,
+      duration: fileDuration,
+    };
+    setMediaShowed(media);
+  };
+
+  const setMediasList = (newMedia) => {
+    setMedias([...medias, ...newMedia]);
+  };
+
+  const DeleteMedia = (mediaPath) => {
+    const newMediasList = medias.filter((media) => media.uri !== mediaPath);
+
+    setMedias(newMediasList);
+  };
+
+  useEffect(() => {
+    if (Object.keys(mediaShowed).length !== 0) {
+      setModalShowMediaVisible(true);
+    }
+  }, [mediaShowed]);
+
+  const renderItem = ({item}) => {
+    if (item.type === 'image/jpeg') {
+      return (
+        <ImagePreview
+          item={item}
+          setOpenedImage={setOpenedImage}
+          setIsVisibleImageModal={setIsVisibleImageModal}
+          DeleteMedia={DeleteMedia}
+        />
+      );
+    }
+
+    if (item.type === 'audio/mpeg') {
+      return (
+        <AudioPreview
+          item={item}
+          handleShowMedia={handleShowMedia}
+          audioCount={audioCount}
+          setAudioCount={setAudioCount}
+          DeleteMedia={DeleteMedia}
+        />
+      );
+    }
+
+    if (item.type === 'application/pdf') {
+      return (
+        <DocumentPreview
+          item={item}
+          handleShowMedia={handleShowMedia}
+          DeleteMedia={DeleteMedia}
+        />
+      );
+    }
+
+    return (
+      <VideoPreview
+        item={item}
+        handleShowMedia={handleShowMedia}
+        DeleteMedia={DeleteMedia}
+      />
+    );
+  };
+
+  return (
+    <>
+      <View px={3}>
+        <View my={2}>
+          <Input
+            label={namePlaceholder}
+            onChange={(value) => setTitle(value)}
+            value={title.value}
+            autoCapitalize="words"
+            onFocus={() => sheetRef.current.snapToIndex(2)}
+            rules={[required]}
+          />
+        </View>
+        {medias.length ? (
+          <View>
+            <Text fontWeight="bold" fontSize={theme.font.sizes.SM} mb={2}>
+              Multimídia
+            </Text>
+            <FlatList
+              mb={1}
+              data={medias}
+              horizontal
+              keyExtractor={(item) => item.uri}
+              renderItem={renderItem}
+            />
+          </View>
+        ) : null}
+        <View>
+          <Input
+            height={100}
+            characterRestriction={5000}
+            maxLength={5000}
+            label={descriptionPlaceholder}
+            onChange={(value) => setDescription(value)}
+            value={description.value}
+            multiline
+          />
+        </View>
+        <Btn
+          onPress={onSave}
+          disabled={!formIsValid()}
+          title={buttonName}
+        />
+      </View>
+      <Fabs actions={actions} />
+      <View>
+        <Modal
+          isVisible={modalVisible}
+          onSwipeComplete={toggleModal}
+          swipeDirection={['down']}
+          style={{justifyContent: 'flex-end', margin: 0}}>
+          <RecordAudioModalContent
+            toggleModal={toggleModal}
+            setMedias={setMediasList}
+            value={audioCount + 1}
+            setAudioCount={() => setAudioCount(audioCount + 1)}
+          />
+        </Modal>
+        <Modal
+          isVisible={modalCamVisible}
+          onSwipeComplete={toggleCamModal}
+          swipeDirection={['down']}
+          style={{justifyContent: 'flex-end', margin: 0}}>
+          <SelectModal
+            toggleModal={toggleCamModal}
+            setMedias={setMediasList}
+          />
+        </Modal>
+        <Modal
+          isVisible={modalMediaVisible}
+          onSwipeComplete={toggleMediaModal}
+          swipeDirection={['down']}
+          style={{justifyContent: 'flex-end', margin: 0}}>
+          <MediaModalContent
+            toggleModal={toggleMediaModal}
+            setMedias={setMediasList}
+          />
+        </Modal>
+        <Modal
+          isVisible={modalShowMediaVisible}
+          style={{justifyContent: 'center', margin: 0}}>
+          <ShowMediaModal
+            media={mediaShowed}
+            closeModal={closeShowMediaModal}
+          />
+        </Modal>
+        <ImageView
+          images={[openedImage]}
+          imageIndex={0}
+          visible={visibleImageModal}
+          onRequestClose={() => setIsVisibleImageModal(false)}
+        />
+      </View>
+    </>
+  );
+};
+
+EditPoint.propTypes = {
+  locationSelected: PropTypes.shape({
+    latitude: PropTypes.number,
+    longitude: PropTypes.number,
+  }),
+  show: PropTypes.bool,
+  onClose: PropTypes.func,
+};
+
+EditPoint.defaultProps = {
+  locationSelected: {},
+  show: false,
+  onClose: () => {},
+};
+
+export default EditPoint;
---
+++ src/components/EditPoint/styles.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components/native';
+import {Platform} from 'react-native';
+import normalize from 'react-native-normalize';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5';
+import theme from 'theme/theme';
+
+export const Icon = styled(FontAwesomeIcon).attrs((props) => ({
+  color: props.color ? props.color : theme.colors.black,
+}))``;
+
+export const Container = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-left: -15px;
+  margin-top: ${Platform.OS === 'ios' ? '-28px' : '-43px'};
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Option = styled.TouchableHighlight.attrs({
+  underlayColor: theme.colors.secondary,
+})`
+  border-radius: 100px;
+  width: ${({size}) => size}px;
+  height: ${({size}) => size}px;
+  background-color: ${theme.colors.primary};
+  align-items: center;
+  justify-content: center;
+  margin: 5px;
+`;
+
+export const MediaButton = styled.TouchableOpacity`
+  width: 100%;
+  height: 70%;
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-top: 5px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MediaContainer = styled.View`
+  width: ${normalize(100)};
+  height: ${normalize(125)};
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-top: 5px;
+  align-items: center;
+  /* background-color: #00F; */
+`;
+
+export const DeleteButton = styled.TouchableOpacity`
+  width: 30%;
+  height: 30%;
+  border-radius: 7px;
+  justify-content: center;
+  /* background-color: #000; */
+  align-items: center;
+`;
