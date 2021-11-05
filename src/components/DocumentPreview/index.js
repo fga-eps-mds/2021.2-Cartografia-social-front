03dd6f86d8581157394faa9382ecac2ea823f04b
Guilherme Banci <guibanci@gmail.com>
2021-11-04 22:58:47 -0300

    [#68] feat - fix document preview
    Co-authored-by: Arthur Rodrigues <arthurarp97@gmail.com>
    Co-authored-by: Marco Antonio <markinlimac@gmail.com>
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Signed-off-by: Guilherme Banci <guibanci@gmail.com>

--- src/components/DocumentPreview/index.js
+++ src/components/DocumentPreview/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 import normalize from 'react-native-normalize';
@@ -8,7 +9,9 @@ const DocumentPreview = ({item, handleShowMedia, DeleteMedia, hasDelete}) => {
   return (
     <MediaContainer bigger={hasDelete || null}>
       <MediaButton
-        onPress={() => handleShowMedia(item.type, item.uri, item.duration)}
+        onPress={() =>
+          handleShowMedia(item.type, item.uri || item.url, item.duration)
+        }
         style={{borderWidth: 1, borderRadius: 7, padding: 5}}>
         <Icon size={normalize(40)} name="file-pdf" color="#2a3c46" />
         <Text style={{fontSize: normalize(15), color: '#2a3c46'}}>PDF</Text>
@@ -19,7 +22,7 @@ const DocumentPreview = ({item, handleShowMedia, DeleteMedia, hasDelete}) => {
         </Text>
       </MediaButton>
       {hasDelete ? (
-        <DeleteButton onPress={() => DeleteMedia(item.uri)}>
+        <DeleteButton onPress={() => DeleteMedia(item.uri || item.id)}>
           <Icon size={normalize(20)} name="trash" color="#FF0000" />
         </DeleteButton>
       ) : null}
