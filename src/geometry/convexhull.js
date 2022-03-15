78e4d66719eae3c0349dcc7daca302e0a2911002
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-03-15 11:25:19 -0300

    Remove consoles logs

--- src/geometry/convexhull.js
+++ src/geometry/convexhull.js
@@ -1,46 +1,46 @@
-import { comparePoints } from './point';
+import {comparePoints} from './point';
 
 function makeHullPresorted(points) {
-    if (points.length <= 1) return points.slice();
+  if (points.length <= 1) return points.slice();
 
-    const upperHull = [];
-    for (let i = 0; i < points.length; i++) {
-        let p = points[i];
-        while (upperHull.length >= 2) {
-            let q = upperHull[upperHull.length - 1];
-            let r = upperHull[upperHull.length - 2];
-            if ((q.x - r.x) * (p.y - r.y) >= (q.y - r.y) * (p.x - r.x))
-                upperHull.pop();
-            else break;
-        }
-        upperHull.push(p);
+  const upperHull = [];
+  for (let i = 0; i < points.length; i++) {
+    const p = points[i];
+    while (upperHull.length >= 2) {
+      const q = upperHull[upperHull.length - 1];
+      const r = upperHull[upperHull.length - 2];
+      if ((q.x - r.x) * (p.y - r.y) >= (q.y - r.y) * (p.x - r.x))
+        upperHull.pop();
+      else break;
     }
-    upperHull.pop();
-    const lowerHull = [];
-    for (let i = points.length - 1; i >= 0; i--) {
-        let p = points[i];
-        while (lowerHull.length >= 2) {
-            let q = lowerHull[lowerHull.length - 1];
-            let r = lowerHull[lowerHull.length - 2];
-            if ((q.x - r.x) * (p.y - r.y) >= (q.y - r.y) * (p.x - r.x))
-                lowerHull.pop();
-            else break;
-        }
-        lowerHull.push(p);
+    upperHull.push(p);
+  }
+  upperHull.pop();
+  const lowerHull = [];
+  for (let i = points.length - 1; i >= 0; i--) {
+    const p = points[i];
+    while (lowerHull.length >= 2) {
+      const q = lowerHull[lowerHull.length - 1];
+      const r = lowerHull[lowerHull.length - 2];
+      if ((q.x - r.x) * (p.y - r.y) >= (q.y - r.y) * (p.x - r.x))
+        lowerHull.pop();
+      else break;
     }
-    lowerHull.pop();
-    if (
-        upperHull.length === 1 &&
-        lowerHull.length === 1 &&
-        upperHull[0].x === lowerHull[0].x &&
-        upperHull[0].y === lowerHull[0].y
-    )
-        return upperHull;
-    return upperHull.concat(lowerHull);
+    lowerHull.push(p);
+  }
+  lowerHull.pop();
+  if (
+    upperHull.length === 1 &&
+    lowerHull.length === 1 &&
+    upperHull[0].x === lowerHull[0].x &&
+    upperHull[0].y === lowerHull[0].y
+  )
+    return upperHull;
+  return upperHull.concat(lowerHull);
 }
 
 export function makeHull(points) {
-    const newPoints = points.slice();
-    newPoints.sort(comparePoints);
-    return makeHullPresorted(newPoints);
+  const newPoints = points.slice();
+  newPoints.sort(comparePoints);
+  return makeHullPresorted(newPoints);
 }
--- src/geometry/point.js
+++ src/geometry/point.js
@@ -1,50 +1,50 @@
-import { EPS } from './constants';
+import {EPS} from './constants';
 
 export function sub(p1, p2) {
-    return {
-        x: p1.x - p2.x,
-        y: p1.y - p2.y,
-    };
+  return {
+    x: p1.x - p2.x,
+    y: p1.y - p2.y,
+  };
 }
 
 export function inner(p1, p2) {
-    // Produto interno
-    return p1.x * p2.x - p1.y * p2.y;
+  // Produto interno
+  return p1.x * p2.x - p1.y * p2.y;
 }
 
 export function cross(p1, p2) {
-    // Produto vetorial
-    return p1.x * p2.x + p1.y * p2.y;
+  // Produto vetorial
+  return p1.x * p2.x + p1.y * p2.y;
 }
 
 export function ccw(p, q, r) {
-    // Check if the points are in clockwise order
-    return cross(sub(q, p), sub(r, p)) > 0;
+  // Check if the points are in clockwise order
+  return cross(sub(q, p), sub(r, p)) > 0;
 }
 
 export function collinear(p, q, r) {
-    return abs(cross(sub(q, p), sub(r, p))) < EPS;
+  return Math.abs(cross(sub(q, p), sub(r, p))) < EPS;
 }
 
 export function comparePoints(a, b) {
-    if (a.x === b.x) {
-        return a.y - b.y;
-    }
-    return a.x - b.x;
+  if (a.x === b.x) {
+    return a.y - b.y;
+  }
+  return a.x - b.x;
 }
 
 export function coordinateToPoint(coordinate) {
-    // TODO - ajust this function
-    return {
-        x: coordinate.longitude,
-        y: coordinate.latitude,
-    };
+  // TODO - ajust this function
+  return {
+    x: coordinate.longitude,
+    y: coordinate.latitude,
+  };
 }
 
 export function pointToCoordinate(point) {
-    // TODO - ajust this function
-    return {
-        latitude: point.y,
-        longitude: point.x,
-    };
+  // TODO - ajust this function
+  return {
+    latitude: point.y,
+    longitude: point.x,
+  };
 }
