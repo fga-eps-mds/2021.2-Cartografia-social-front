c2e6333e99655668130f8f84dbf426ffb3576f89
Gustavo Martins <72039007+gustavomartins-github@users.noreply.github.com>
2022-03-26 13:43:10 -0300

    Atualização do localhost
    Atualizando o localhost para correto funcionamento da API

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ axios.defaults.timeout = DEFAULT_TIMEOUT;
 
 const instance = axios.create({
   // baseURL: Config.API,
-  baseURL: 'http://147.182.225.67:8000',
+  baseURL: 'http://192.168.1.14:8000',
 });
 
 instance.interceptors.request.use(async (config) => ({
