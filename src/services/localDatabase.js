f70cae9d3ac8f54b48c085798dd001673e495727
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-03-26 15:14:54 -0300

    Creation of basic functions for storage
    Co-authored-by: Vinícius Alves de Sousa <viniciusalves999@gmail.com>
    Co-authored-by: Eric chagas <eric_chagas12@outlook.com>

--- src/services/localDatabase.js
+++ src/services/localDatabase.js
@@ -40,3 +40,54 @@ const getEntityArray = async (entityName) => {
     else throw e;
   }
 }
+
+export const exists = async (entityName, id) => {
+  try {
+    getData(`${entityName}_${id}`);
+    return true;
+  }
+  catch (e) {
+    if (e.message === "No data found") {
+      return false;
+    }
+    else throw e;
+  }
+}
+
+export const put = async (entityName, data) => {
+  if (!data.id) {
+    data.id = Math.random().toString(36).substring(7);
+  }
+  let entityArray = await getEntityArray(entityName);
+  console.log('entityArray', entityArray)
+  if (await exists(entityName, data.id))
+    entityArray = entityArray.map(e => e.id === data.id ? data : e)
+  else
+    entityArray.push(data);
+
+  storeData(`${entityName}_${data.id}`, data);
+  storeData(entityName, entityArray);
+}
+
+export const post = async (entityName, data) => {
+  if (await exists(entityName, data.id)) {
+    throw new Error("Entity already exists");
+  }
+  await put(entityName, data);
+}
+
+export const get = async (entityName, id) => {
+  return getData(`${entityName}_${id}`);
+}
+
+export const remove = async (entityName, id) => {
+  let entityArray = await getEntityArray(entityName);
+  console.log('entityArray', entityArray)
+  storeData(entityName, entityArray.filter(e => e.id !== id));
+  deleteData(`${entityName}_${id}`);
+}
+
+export const getAll = async (entityName) => {
+  return getData(entityName);
+}
+
