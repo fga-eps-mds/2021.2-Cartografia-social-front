7ad1ff37ec6b68ca1e484acae3c8d5c41deb4377
Eric-chagas <eric_chagas12@outlook.com>
2022-03-27 21:09:55 -0300

    Fixing linter
    Co-authored-by: gustavolima00 <gustavomarques.gml@gmail.com>
    Co-authored-by: viniciusalves999 <viniciusalves999@gmail.com>

--- src/services/offlineLogin.js
+++ src/services/offlineLogin.js
@@ -1,20 +1,20 @@
 import * as localDataBase from './localDatabase';
 
-export const storeLocalCredentials = async (/*userName, password*/) => {
+export const storeLocalCredentials = async (userName, password) => {
   await localDataBase.put('offlineLoginCredentials', {
     id: '',
-    //username: userName,
-    //password,
+    username: userName,
+    password,
   });
-  // console.log('Salvo!');
+  //console.log('Salvo!');
 };
 
-export const offlineLogin = async (userName, password) => {
-  // const data = await localDataBase.getAll('offlineLoginCredential');
-  /* for (key in data) {
+export const offlineLogin = async (/*userName, password*/) => {
+  //const data = await localDataBase.getAll('offlineLoginCredential');
+  /*for (key in data) {
     if (key.username === userName && key.password === password) {
       return true;
     }
-  } */
+  }*/
   return false;
 };
