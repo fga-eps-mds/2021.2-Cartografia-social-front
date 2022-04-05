2fc40c72ad595d5a39eb9cb104c5e0bbec15e67f
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-04-05 15:51:31 -0300

    Creation of function postArea
    Co-authored-by: Vinícius Alves de Sousa <viniciusalves999@gmail.com>
    Co-authored-by: Eric chagas <eric_chagas12@outlook.com>

--- src/services/offlineMapService.js
+++ src/services/offlineMapService.js
@@ -1,7 +1,17 @@
 import uuid from 'react-native-uuid';
 import * as localDatabase from './localDatabase';
+import api from './api';
 
 const AREA_ENTITY = 'area';
+
+const postArea = async (area, userEmail) => {
+    const response = await api.post('/maps/area', area)
+    const locationId = response.data.id;
+    await api.post('/maps/addToCommunity', {
+        locationId: locationId,
+        userEmail: userEmail,
+    })
+}
 
 export const saveArea = async (area) => localDatabase.post(AREA_ENTITY, {
     ...area,
@@ -9,3 +19,7 @@ export const saveArea = async (area) => localDatabase.post(AREA_ENTITY, {
 });
 
 export const getAreas = async () => localDatabase.getAll(AREA_ENTITY);
+
+export const syncAreas = async (areas) => {
+
+}
