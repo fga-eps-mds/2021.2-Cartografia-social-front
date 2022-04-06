dacc37cbe7aea71b8261371a579c0b765daf9a6c
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-04-06 11:48:48 -0300

    Addition of get local info data
    Co-authored-by: Vinícius Alves de Sousa <viniciusalves999@gmail.com>
    Co-authored-by: Eric chagas <eric_chagas12@outlook.com>

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,17 +1,39 @@
 import auth from '@react-native-firebase/auth';
-import * as localDatabase from './localDatabase';
 import AsyncStorage from '@react-native-community/async-storage';
+import api from 'services/api';
+import * as localDatabase from 'services/localDatabase';
+
+export const USER_ENTITY = 'user';
+
+const getUserInfo = async (email) => {
+  const result = await api.get(
+    'users/userByEmail',
+    {
+      params: {
+        email: email.value,
+      },
+    },
+    {
+      headers: {
+        Authorization: await AsyncStorage.getItem('access_token'),
+      },
+    },
+  );
+  return result.data;
+};
 
 const firebaseLogin = async (email, password) => {
-    const userCredentials = await auth().signInWithEmailAndPassword(
-        email.trim(),
-        password.trim(),
-    );
-    return await userCredentials.user.getIdToken();
-}
+  const userCredentials = await auth().signInWithEmailAndPassword(
+    email.trim(),
+    password.trim(),
+  );
+  return userCredentials.user.getIdToken();
+};
 
 export const login = async (email, password) => {
-    const token = await firebaseLogin(email, password);
-    await AsyncStorage.setItem('access_token', `Bearer ${token}`);
-    return {}
-}
+  const token = await firebaseLogin(email, password);
+  await AsyncStorage.setItem('access_token', `Bearer ${token}`);
+  const userInfo = await getUserInfo(email);
+  await localDatabase.put(USER_ENTITY, userInfo);
+  return userInfo;
+};
--- test/services/loginService.spec.js
+++ test/services/loginService.spec.js
@@ -1,5 +1,7 @@
-import { login } from '../../src/services/loginService';
+import { login, USER_ENTITY } from '../../src/services/loginService';
 import AsyncStorage from '@react-native-community/async-storage';
+import mockAxios from 'jest-mock-axios';
+import * as localDatabase from '../../src/services/localDatabase';
 
 jest.mock('@react-native-firebase/auth', () => {
     return () => ({
@@ -11,6 +13,15 @@ jest.mock('@react-native-firebase/auth', () => {
         }),
     })
 });
+const mockUserInfo = {
+    "email": "email",
+    "name": "name",
+    "cellPhone": "cellPhone",
+    "type": "type",
+    "uid": "uid",
+    "id": "id",
+    "imageUrl": "imageUrl"
+}
 
 const correctUserData = {
     email: 'a@a.com',
@@ -18,7 +29,9 @@ const correctUserData = {
 }
 describe('loginService', () => {
     it('can login correctly', async () => {
-        await expect(login(correctUserData.email, correctUserData.password)).resolves.toStrictEqual({});
+        mockAxios.get.mockResolvedValue({ data: mockUserInfo });
+        await expect(login(correctUserData.email, correctUserData.password)).resolves.toStrictEqual(mockUserInfo);
         await expect(AsyncStorage.getItem('access_token')).resolves.toStrictEqual('Bearer token');
+        await expect(localDatabase.get(USER_ENTITY, mockUserInfo.id)).resolves.toStrictEqual(mockUserInfo);
     })
 })
