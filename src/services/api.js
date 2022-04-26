e55d07ea3cfebc2dea44bf2b7f302bcee420da46
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-04-26 18:54:08 -0300

    :art:

--- src/services/api.js
+++ src/services/api.js
@@ -6,38 +6,38 @@ const DEFAULT_TIMEOUT = 30000;
 axios.defaults.timeout = DEFAULT_TIMEOUT;
 
 const instance = axios.create({
-    // baseURL: Config.API,
-    baseURL: 'http://192.168.1.10:8000',
+  // baseURL: Config.API,
+  baseURL: 'http://192.168.1.10:8000',
 });
-instance.interceptors.request.use(async(config) => ({
-    ...config,
-    headers: {
-        ...config.headers,
-        Authorization: await AsyncStorage.getItem('access_token'),
-    },
+instance.interceptors.request.use(async (config) => ({
+  ...config,
+  headers: {
+    ...config.headers,
+    Authorization: await AsyncStorage.getItem('access_token'),
+  },
 }));
 
 instance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        const originalRequest = error.config;
-        if (!error.response) {
-            throw error
-        }
-        if (error.response.status === 401 && !originalRequest._retry) {
-            originalRequest._retry = true;
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const originalRequest = error.config;
+    if (!error.response) {
+      throw error;
+    }
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
 
-            return auth()
-                .currentUser.getIdToken(true)
-                .then(async(token) => {
-                    await AsyncStorage.setItem('access_token', `Bearer ${token}`);
-                    return axios(originalRequest);
-                });
-        }
-        throw error;
-    },
+      return auth()
+        .currentUser.getIdToken(true)
+        .then(async (token) => {
+          await AsyncStorage.setItem('access_token', `Bearer ${token}`);
+          return axios(originalRequest);
+        });
+    }
+    throw error;
+  },
 );
 
 export default instance;
